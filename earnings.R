385ddbd53852229d9cf010236923ba3b033c9bd8
jaymokimbooth <jaymo.kim@chicagobooth.edu>
2020-09-15 19:42:16 +0000

    Output of regression now stores in a txt file.

--- earnings.R
+++ earnings.R
@@ -1,4 +1,7 @@
 earnings <- read.csv("https://raw.githubusercontent.com/avehtari/ROS-Examples/master/Earnings/data/earnings.csv")
 
+sink("reg01.txt", append = FALSE)
 fitted.model <- lm(earnings$earn ~ earnings$height)
+print(Sys.date(), quote = FALSE)
 print(summary(fitted.model))
+closeAllConnections()
