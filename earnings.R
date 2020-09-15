1701eeded8f2616ca6f15c7f8f00ea739b235bd7
jaymokimbooth <jaymo.kim@chicagobooth.edu>
2020-09-15 20:52:21 +0000

    Initial attempts at regressions with a log transformation.

--- earnings.R
+++ earnings.R
@@ -42,5 +42,54 @@ interaction.model <- function(){
     dev.off() # close the file
     }
 
+# 
+reg04.model <- function(){
+    earnings <- subset(earnings, earn>0)
+    fitted.model <- lm(log(earnings$earn) ~ earnings$height + earnings$male)
+    writeOutput(fitted.model, 'reg04.txt')
+    png('rego4.png')
+    plot(earnings$height, earnings$earn, xlim = c(40, 90), xlab = "height", ylab = "earnings")
+    abline(fitted.model, col = "red") # plot the regression line
+    dev.off() # close the file
+    }
+
+# 
+reg05.model <- function(){
+    earnings <- subset(earnings, earn>0)
+    fitted.model <- lm(log(earnings$earn) ~ earnings$height*earnings$male)
+    writeOutput(fitted.model, 'reg05.txt')
+    png('reg05.png')
+    plot(earnings$height, earnings$earn, xlim = c(40, 90), xlab = "height", ylab = "earnings")
+    abline(fitted.model, col = "red") # plot the regression line
+    dev.off() # close the file
+    }
+
+# 
+reg06.model <- function(){
+    earnings <- subset(earnings, earn>0)
+    fitted.model <- lm(log(earnings$earn) ~ log(earnings$height) + earnings$male)
+    writeOutput(fitted.model, 'reg06.txt')
+    png('reg06.png')
+    plot(earnings$height, earnings$earn, xlim = c(40, 90), xlab = "height", ylab = "earnings")
+    abline(fitted.model, col = "red") # plot the regression line
+    dev.off() # close the file
+    }
+
+# 
+reg07.model <- function(){
+    earnings <- subset(earnings, earn>0)
+    fitted.model <- lm(log(earnings$earn) ~ log(earnings$height)*earnings$male)
+    writeOutput(fitted.model, 'reg07.txt')
+    png('reg07.png')
+    plot(earnings$height, earnings$earn, xlim = c(40, 90), xlab = "height", ylab = "earnings")
+    abline(fitted.model, col = "red") # plot the regression line
+    dev.off() # close the file
+    }
+
+
 male.model()
 interaction.model()
+reg04.model()
+reg05.model()
+reg06.model()
+reg07.model()
