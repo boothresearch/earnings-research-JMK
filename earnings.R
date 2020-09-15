a70815a726cd55745e16b3e3e769cded0788a23d
jaymokimbooth <jaymo.kim@chicagobooth.edu>
2020-09-15 19:54:37 +0000

    included visualization and saved on .png file

--- earnings.R
+++ earnings.R
@@ -5,3 +5,9 @@ fitted.model <- lm(earnings$earn ~ earnings$height)
 print(Sys.time(), quote = FALSE)
 print(summary(fitted.model))
 closeAllConnections()
+
+# graph the data and the regression line
+png("reg01.png") # open a png file
+plot(earnings$height, earnings$earn, xlim = c(40, 90), xlab = "height", ylab = "earnings")
+abline(fitted.model, col = "red") # plot the regression line
+dev.off() # close the file
